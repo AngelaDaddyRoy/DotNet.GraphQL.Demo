e7d0dec44f54cbe2332403ee4f3bf9f1b4d29b4b
roy <jnmcluo@163.com>
2019-04-20 10:48:59 +0800

    first commit

---
+++ GraphQLTest/Queries/Types/CommentType.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+using GraphQLTest.Entities;
+
+namespace GraphQLTest.Queries.Types
+{
+    public class CommentType : ObjectGraphType<Comment>
+    {
+        public CommentType()
+        {
+            Field(x => x.Count);
+            Field(x => x.Description);
+            Field(x => x.Url);
+        }
+    }
+}
---
+++ GraphQLTest/Queries/Types/RatingType.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+using GraphQLTest.Entities;
+
+namespace GraphQLTest.Queries.Types
+{
+    public class RatingType : ObjectGraphType<Comment>
+    {
+        public RatingType()
+        {
+            Field(x => x.Count);
+            Field(x => x.Description);
+            Field(x => x.Url);
+        }
+    }
+}
